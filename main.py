eb2dcdbb060d186e42cd247cf6324514f6c34bd1
aaradhyajajoo <38855824+aaradhyajajoo@users.noreply.github.com>
2023-01-30 18:31:09 -0500

    Basic idea what main.py would look like

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
 #!/usr/bin/ python3
 
-def main():
+import sys
+import os 
+
+def main(*args, **kwargs):
+
+    # no arguments provided
+    if (len(args) == 0):
+        sys.exit("No arguments provided")
     
-    print()
+    # check if the first argument is install or test
+    if (args[0] == "install"):
+        # install function to be called here
+        print("install")
+    elif (args[0] == "test"):
+        # test function to be called here
+        print("test")
+    
+    # default test: check if the files exist
+    else:
+        check_files_exists(*args, **kwargs)
+
+def check_files_exists(*args, **kwargs):
+    for arg in args:
+        if not os.path.exists(str(arg)):
+            sys.exit("File {} does not exist".format(arg))
+    
+if __name__ == "__main__":
+    main(sys.argv[1:])
