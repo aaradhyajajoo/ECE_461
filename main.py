d9f733d05aeb173e4882cb34d9204aae168b2000
tanvik <tanvikalaskar11@gmail.com>
2023-01-30 19:22:39 -0500

    Check

--- main.py
+++ main.py
@@ -1,4 +1,17 @@
 #!/usr/bin/ python3
+import sys
+import subprocess
+import os
 
-def main():
-    print("Hello World")
+def main(*args, **kwargs):
+
+
+    if (args[0] == "install"):
+     os.system("pip install -r requirements.txt")
+     sys.stdout = open("/dev/null", "w")
+
+
+
+    
+    
+
