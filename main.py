a183e2bfc76beef0df3e61c4c53d8cb0fdc2ca44
aaradhyajajoo <38855824+aaradhyajajoo@users.noreply.github.com>
2023-02-09 12:07:03 -0500

    working for the 3 github links

--- main.py
+++ main.py
@@ -8,7 +8,8 @@ def install():
     os.system("npm --silent --no-progress install")
     os.system("npm --silent --no-progress install -g typescript")
     os.system("npm --silent --no-progress install -g ts-node")
-    os.system("pip install -r requirements.txt > /dev/null 2>&1")
+    os.system("npm --silent --no-progress install -g ts-node-dev")
+    os.system("pip install -q -r requirements.txt > /dev/null 2>&1")
     os.system("ts-node src/install.ts > /dev/null 2>&1")
     os.system("node src/install.js")
     sys.exit(0)
@@ -26,11 +27,14 @@ def main(args, *kwargs):
     elif (args[0].strip() == "test"):
         # test function to be called here
         print("test")
+        sys.exit(0)
+
     # default test: check if the files exist
     else:
         check_files_exists(args, *kwargs)
         os.system(f"ts-node src/graph_api_call.ts {args[0]}")
         os.system(f"node src/graph_api_call.js {args[0]}")
+        sys.exit(0)
 
 # check if the files with the input path exist
 def check_files_exists(args):
--- src/install.js
+++ src/install.js
@@ -1,4 +1,4 @@
 var json = require("../package.json"); // Package json as a json object
 var dependencies = json.dependencies; // Dependencies as a json object
-var count = 1 + Object.keys(dependencies).length; // Number of dependencies
+var count = 4 + Object.keys(dependencies).length; // Number of dependencies
 console.log(count + " dependencies installed.");
