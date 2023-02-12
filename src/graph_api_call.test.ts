afaa1bdfeebac389a3085d0bff662436302c9318
Eshaan Minocha <eminocha@eceprog2.ecn.purdue.edu>
2023-02-12 12:51:54 -0500

    CI testing

--- src/graph_api_call.test.ts
+++
@@ -1,7 +0,0 @@
-import { write_to_log_file } from './graph_api_call';
-// import { ramp_upTime_calc } from './graph_api_call';
-
-test('write_to_log_file returns 0', () => {
-    const result = write_to_log_file();
-    expect(result).toBe(0);
-});
